73978a2d1b8195fb368341f0844ceb9073feb628
Troy Whiteley <troy@somany.us>
2016-03-14 14:20:18 -0700

    Fix desktop check and continue early

--- index.js
+++ index.js
@@ -12,64 +12,67 @@ exports.register = function(server, options, next) {
     path: '/'
   });
 
-  server.ext('onPreResponse', (request, reply) => {
-    if (request.response.variety !== 'view') {
-      return reply.continue();
-    }
-
-    if (request.query.ftDeviceType === 'desktop') {
-      const query = request.url;
-
-      query.search = '';
-      delete query.query.ftDeviceType;
-
-      reply.state('ftDeviceType', 'desktop');
-
-      return reply.redirect(url.format(query))
-                  .temporary(true)
-                  .rewritable(false);
-    }
-
-    if (!request.response.source.context) {
-      request.response.source.context = {};
-    }
-
-    const context = request.response.source.context;
-
-    if (request.state.ftDeviceType !== 'desktop') {
-      const userAgent = uaParser(request.headers['user-agent']);
-      context.__isMobile = (userAgent.device.type === 'mobile' || userAgent.ua.indexOf('IEMobile') !== -1);
-    } else {
-      context.__isMobile = false;
-    }
-
-    if (!context.__isMobile) {
-      return reply.continue();
-    }
-
-    const templatePath = request.response.source.compiled.settings.path;
-    let template = request.response.source.template;
-
-    if (context.__isMobile) {
-      template = `${template}-mobile`;
-    }
-
-    fs.stat(path.join(templatePath, `${template}.html`), (err, stat) => {
-      if (err || !stat.isFile()) {
+  server.ext({
+    type: 'onPreResponse',
+    method: (request, reply) => {
+      if (request.response.variety !== 'view') {
         return reply.continue();
       }
 
-      const response = reply.view(template, context);
-      response.vary('User-Agent');
+      if (request.query.ftDeviceType === 'desktop') {
+        const query = request.url;
 
-      const headers = request.response.headers;
+        query.search = '';
+        delete query.query.ftDeviceType;
 
-      for (const header of Object.keys(headers)) {
-        response.header(header, headers[header]);
+        reply.state('ftDeviceType', 'desktop');
+
+        return reply.redirect(url.format(query))
+                    .temporary(true)
+                    .rewritable(false);
       }
 
-      response.code(request.response.statusCode);
-    });
+      if (!request.response.source.context) {
+        request.response.source.context = {};
+      }
+
+      const context = request.response.source.context;
+
+      if (request.state.ftDeviceType !== 'desktop') {
+        const userAgent = uaParser(request.headers['user-agent']);
+        context.__isMobile = (userAgent.device.type === 'mobile' || userAgent.ua.indexOf('IEMobile') !== -1);
+      } else {
+        context.__isMobile = false;
+      }
+
+      if (!context.__isMobile) {
+        return reply.continue();
+      }
+
+      const templatePath = request.response.source.compiled.settings.path;
+      let template = request.response.source.template;
+
+      if (context.__isMobile) {
+        template = `${template}-mobile`;
+      }
+
+      fs.stat(path.join(templatePath, `${template}.html`), (err, stat) => {
+        if (err || !stat.isFile()) {
+          return reply.continue();
+        }
+
+        const response = reply.view(template, context);
+        response.vary('User-Agent');
+
+        const headers = request.response.headers;
+
+        for (const header of Object.keys(headers)) {
+          response.header(header, headers[header]);
+        }
+
+        response.code(request.response.statusCode);
+      });
+    }
   });
 
   return next();
